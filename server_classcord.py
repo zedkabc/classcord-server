e27c1cfb76848654a1c2a657972c17ce8bdb8bc4
zedkabc <lavenirlouka@gmail.com>
2025-06-16 15:45:41 +0200

    maj du code

--- server_classcord.py
+++ server_classcord.py
@@ -6,7 +6,7 @@ import pickle
 import os
 from datetime import datetime
 
-HOST = '127.0.0.1'
+HOST = '0.0.0.0'
 PORT = 12345
 
 USER_FILE = 'users.pkl'
@@ -96,12 +96,13 @@ def handle_client(client_socket):
 def main():
     load_users()
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('0.0.0.0', 12345))
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((HOST, PORT))
     server_socket.listen()
     print(f"[DEMARRAGE] Serveur en écoute sur {HOST}:{PORT}")
     while True:
         client_socket, addr = server_socket.accept()
         threading.Thread(target=handle_client, args=(client_socket,), daemon=True).start()
 
-if __name__ == '__main__':
+if name == 'main':
     main()
